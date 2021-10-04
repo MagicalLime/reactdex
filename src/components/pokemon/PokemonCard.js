4856c92d407c498af55b940877b41953de55e187
Abijit Rangesh <rangeshabijit@gmail.com>
2021-10-04 18:34:04 -0400

    Added hover feature to cards and fixed bugs with image retrieval from github

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -1,17 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import spinner from './spinner.gif';
+
+const Sprite = styled.img`
+    width: 5em;
+    height: 5em;
+    display: none;
+`;
+
+const Card = styled.div`
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgb(0, 0, 0, 0.24);
+    transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+    &:hover {
+        box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22); 
+    }
+`;
 
 export default class PokemonCard extends Component {
 
     state = {
         name: '',
         imageUrl: '',
-        pokemonIndex: ''
+        pokemonIndex: '',
+        imageLoading: true,
+        tooManyReqs: false
     }
 
     componentDidMount() {
         const { name, url } = this.props;
         const pokemonIndex = url.split('/')[url.split('/').length - 2];
         const imageUrl = `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${pokemonIndex}.png?raw=true`
+        //const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonIndex}.png`
 
         this.setState({
             name,
@@ -27,17 +46,36 @@ export default class PokemonCard extends Component {
             // column medium 3 says 12 / 3 = 4 cards in medium size mode
             // column small 6 says 12 / 6 = 2 cards in small size mode (like if they're on a phone)
             <div className='col-md-3 cold-sm-6 mb-5'>
-                <div className="card">
+                <Card className="card">
                     <h5 className="card-header">{this.state.pokemonIndex}</h5>
-                    <div class="card-body mx-auto">
+                    {this.state.imageLoading ? (
+                        <img
+                            src={spinner}
+                            style={{ width: '5em', height: '5em' }}
+                            className="card-img-top rounded mx-auto d-block mt-2"></img>
+                    ) : null}
+                    <Sprite
+                        className="card-img-top rounded mx-auto nt-2"
+                        onLoad={() => this.setState({ imageLoading: false })}
+                        onError={() => this.setState({ tooManyReqs: true })}
+                        src={this.state.imageUrl}
+                        style={
+                            this.state.tooManyReqs ? { display: 'none' } :
+                                this.state.imageLoading ? null : { display: 'block' }
+                        }
+                    />
+                    {this.state.tooManyReqs ? (<h6 className="mx-auto">
+                        <span className="badge" badge-danger mt-2>Too Many Requests</span>
+                    </h6>) : null}
+                    <div className="card-body mx-auto">
                         <h6 className="card-title">
-                            {this.state.name.split("-").map(str => 
+                            {this.state.name.split("-").map(str =>
                                 str.charAt(0).toUpperCase() + str.substring(1)
                             ).join(" ")
                             }
                         </h6>
                     </div>
-                </div>
+                </Card>
             </div>
         )
     }
